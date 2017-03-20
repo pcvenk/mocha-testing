65e43b801f1b0151794f73d27b2edaac2cd72226
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-03-20 09:08:04 +0100

    Test for finding a resource and update one of its properties

--- src/user.js
+++ src/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 let userSchema = new mongoose.Schema({
-    name: String
+    name: String,
+    postCount: Number
 });
 
 let User = mongoose.model('User', userSchema);
--- test/update-test.js
+++ test/update-test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({name: 'Joe'});
+        joe = new User({name: 'Joe', postCount: 0});
         joe.save()
             .then(() => done());
     });
@@ -31,14 +31,24 @@ describe('Updating records', () => {
     });
 
     it('Updating model class (all instances matching the given criteria', (done) => {
-       assertName(User.update({name: 'Joe'}, {name: 'Magnifico'}), done);
+        assertName(User.update({name: 'Joe'}, {name: 'Magnifico'}), done);
     });
 
     it('Updating model class (findByIdAndUpdate)', (done) => {
-       assertName(User.findByIdAndUpdate(joe._id, {name: 'Magnifico'}), done);
+        assertName(User.findByIdAndUpdate(joe._id, {name: 'Magnifico'}), done);
     });
 
     it('Updating model class (findOneAndUpdate', (done) => {
-       assertName(User.findOneAndUpdate({name: 'Joe'}, {name: 'Magnifico'}), done);
+        assertName(User.findOneAndUpdate({name: 'Joe'}, {name: 'Magnifico'}), done);
+    });
+
+    it('Updating a model class property with the update operator', (done) => {
+        //use of inc update operator
+        User.update({name: 'Joe'}, {$inc: {postCount: 1}})
+            .then(() => User.findOne({name: 'Joe'}))
+            .then((user) => {
+                assert(user.postCount === 1);
+                done();
+            });
     });
 });
