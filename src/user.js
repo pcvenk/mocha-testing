393e8328e495d6b910756b0857d37a57568ba638
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-03-20 09:40:11 +0100

    Added a required propery to the schema

--- src/user.js
+++ src/user.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 let userSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
     postCount: Number
 });
 
