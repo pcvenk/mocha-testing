c853af77a4735757d0ccf77619bda99e35c82ce0
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-03-20 10:46:33 +0100

    Validation test if a name is longer than 2 charcters

--- src/user.js
+++ src/user.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 let userSchema = new mongoose.Schema({
     name: {
         type: String,
+        validate: {
+            validator: (name) => name.length > 2,
+            message: 'Name must be longer than 2 characters'
+        },
         required: [true, 'Name is required']
     },
     postCount: Number
--- test/validate.js
+++ test/validate.js
@@ -2,12 +2,21 @@ const assert = require('assert');
 const User  = require('../src/user');
 
 describe('Recrods validation', () => {
-   it('Requires a user name', (done) => {
-       const user = new User({name: undefined});
-       const validationRes = user.validateSync();
-       const {message} = validationRes.errors.name;
+    it('Requires a user name', (done) => {
+        const user = new User({name: undefined});
+        const validationRes = user.validateSync();
+        const {message} = validationRes.errors.name;
 
-       assert(message === 'Name is required');
-       done();
-   });
+        assert(message === 'Name is required');
+        done();
+    });
+
+    it('Requires a name longer than 2 characters', (done) => {
+        const user = new User({name: 'Al'});
+        const validationRes = user.validateSync();
+        const {message} = validationRes.errors.name;
+
+        assert(message === 'Name must be longer than 2 characters');
+        done();
+    });
 });
