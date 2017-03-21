9f82781bfdab1908c15802d04a84faf89b8e6563
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-03-21 18:28:26 +0100

    Inserted the User, BlogPost and Comment model in the beforeEach fn

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const User     = require('../src/user');
+const BlogPost = require('../src/blogPost');
+const Comment  = require('../src/comment');
 
 mongoose.Promise = global.Promise;
 
@@ -25,6 +27,10 @@ before((done) => {
 
 beforeEach((done) => {
    User.remove({}, () => {
-      done();
+      BlogPost.remove({}, () =>{
+          Comment.remove({}, () =>{
+              done();
+          })
+      })
    });
 });
